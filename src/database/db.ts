532adce01cd38560ca1d791db84a41354d5edf1b
Luiz Ricardo Brugnera <luiz.r.brugnera@gmail.com>
2024-10-04 22:22:52 -0300

    test

--- src/database/db.ts
+++ src/database/db.ts
@@ -9,7 +9,7 @@ console.log(process.env.DB_PASSWORD);
 console.log(process.env.DB_NAME);
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
+  host: "127.0.0.1",
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
